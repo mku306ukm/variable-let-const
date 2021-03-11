3d4aa0adaf6c5627a72b4d20af7ad7d5e1a1cb50
samod singh <samod0098@gmail.com>
2021-03-11 16:01:15 +0530

    variable-let-const

---
+++ index.js
@@ -0,0 +1,32 @@
+
+console.log('ram');
+
+var name = 'shyam';
+var channel;
+channel = 'code with harry';
+var marks =500;
+console.log(name,channel,marks);
+
+/*
+rules for creating javascript variables
+1.cannot start with numbers
+2.can start with letter,numbers, _ or $
+3.are case sensitive
+*/
+var city = 'delhi';
+console.log(city);
+
+const ownerName = 'hari';
+
+console.log(ownerName);
+
+{
+let city = 'ranchi';
+city = 'kolkata';
+console.log(city);
+}
+console.log(city);
+
+const arr1 = [12,23,12,53];
+arr1.push(50);
+console.log(arr1)
